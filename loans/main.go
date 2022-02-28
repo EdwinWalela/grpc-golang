a3e327f1466dd195c7541086a8612b9023b8b601
edwinwalela <edwinwalela@gmail.com>
2022-02-28 09:40:30 +0300

    user grpc loans client

--- loans/main.go
+++ loans/main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/grpc-demo/protos/userproto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:7000"
+)
+
 func main() {
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("failed to connect to grpc server: %v", err)
+
+	}
+
+	defer conn.Close()
+
+	client := pb.NewUserManagmentClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	userReq := &pb.UserReq{
+		Id: 1,
+	}
+
+	user, err := client.GetUser(ctx, userReq)
+
+	if err != nil {
+		log.Fatalf("Failed to get user: %v", err)
+	}
+
+	log.Printf("Recieved user: id:%d name:%s", user.GetId(), user.GetName())
 
 }
